ce98f9c3d718fa8fd12e43de8e59a794fbdc6927
Ranjan02/ <ranjanmaji098@gmail.com>
2023-03-10 02:34:42 +0530

    add logic and style in numberGussingGame

--- numberGuessingGame/script.js
+++ numberGuessingGame/script.js
@@ -1,4 +1,23 @@
+const buttom = document.getElementById("check");
+
 document.querySelector("form").onsubmit = (e) => {
   e.preventDefault();
-  const input = document.getElementById("value").value;
+  const inputValue = document.getElementById("value").value;
+  if (!inputValue) {
+    return;
+  }
+  const rendomNumber = rendomNumberGenarator();
+
+  if (Number(inputValue) === rendomNumber) {
+    document.getElementById("result").innerHTML =
+      "congratulation ! your guessing the number is right";
+  } else {
+    document.getElementById("result").innerHTML =
+      "your guessing the number is worng actually is number is " + rendomNumber;
+  }
+};
+
+const rendomNumberGenarator = () => {
+  const number = Math.floor(Math.random() * 10);
+  return number;
 };
